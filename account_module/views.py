e5b4916ac5d87325989e5489e4ce1fc95cf1e316
Erfan <erfansafari.b.86@gmail.com>
2025-09-21 19:01:15 +0330

    edit template for appear the register form

--- account_module/views.py
+++ account_module/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from account_module.forms import RegisterForm
+
 
 # Create your views here.
 
 class RegisterView(View):
     def get(self, request):
-        return render(request,'account_module/register.html')
+        register_form = RegisterForm()
+        context = {'form': register_form}
+        return render(request, 'account_module/register.html', context)
     def post(self, request):
         pass
