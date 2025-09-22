1850dd09ba74243e6bfbc21c26b3c03f7cccfcb9
Erfan <erfansafari.b.86@gmail.com>
2025-09-22 12:12:36 +0330

    register operation is finish

--- account_module/forms.py
+++ account_module/forms.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django import forms
-from django.contrib.auth.models import User
+from account_module.models import User
 
 
 class RegisterForm(forms.ModelForm):
@@ -59,17 +59,3 @@ class RegisterForm(forms.ModelForm):
         if password and confirm_password and password != confirm_password:
             raise forms.ValidationError("Passwords do not match!")
         return confirm_password
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        y = int(self.cleaned_data['year'])
-        m = int(self.cleaned_data['month'])
-        d = int(self.cleaned_data['day'])
-        user.birth_date = datetime.date(y, m, d)
-
-        user.username = self.cleaned_data['email']
-        user.set_password(self.cleaned_data['password'])
-
-        if commit:
-            user.save()
-        return user
--- account_module/views.py
+++ account_module/views.py
@@ -1,7 +1,11 @@
+import datetime
+
 from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 from django.views.generic import View
 
 from account_module.forms import RegisterForm
+from account_module.models import User
 
 
 # Create your views here.
@@ -11,9 +15,23 @@ class RegisterView(View):
         register_form = RegisterForm()
         context = {'form': register_form}
         return render(request, 'account_module/register.html', context)
+
     def post(self, request):
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("login")
-        return render(request, 'account_module/register.html', {'form': form})
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            if User.objects.filter(email__iexact=email).exists():
+                form.add_error('email', 'Email already registered.')
+            else:
+                new_user = User(email=email, username=email, active_email_code=get_random_string(48), is_active=False)
+                new_user.set_password(password)
+                new_user.first_name=form.cleaned_data['first_name']
+                new_user.last_name=form.cleaned_data['last_name']
+                y,m,d=int(form.cleaned_data['year']), int(form.cleaned_data['month']), int(form.cleaned_data['day'])
+                new_user.birth_date=datetime.date(y,m,d)
+                new_user.gender=form.cleaned_data['gender']
+                new_user.save()
+
+
