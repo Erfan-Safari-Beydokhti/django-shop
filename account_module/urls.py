a41685f0773d8d36bbf97932463bc40f6720654b
Erfan <erfansafari.b.86@gmail.com>
2025-09-24 15:47:38 +0330

    add url for forgot pass view

--- account_module/urls.py
+++ account_module/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView, ActivateView, LoginView
+from .views import RegisterView, ActivateView, LoginView, ForgotPasswordView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login_view'),
     path('register/',RegisterView.as_view(),name='register_view'),
     path('active_code/<email_active_code>/', ActivateView.as_view(), name='active_code'),
+    path('forgot_pass/', ForgotPasswordView.as_view(), name='active_code'),
+
 ]
