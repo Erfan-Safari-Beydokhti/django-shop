556f6615881e948976541b600fb593457721402c
Erfan <erfansafari.b.86@gmail.com>
2025-09-24 16:17:19 +0330

    import template for reset pass page

--- account_module/urls.py
+++ account_module/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RegisterView, ActivateView, LoginView, ForgotPasswordView
+from .views import RegisterView, ActivateView, LoginView, ForgotPasswordView, ResetPasswordView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login_view'),
     path('register/',RegisterView.as_view(),name='register_view'),
     path('active_code/<email_active_code>/', ActivateView.as_view(), name='active_code'),
     path('forgot_pass/', ForgotPasswordView.as_view(), name='forgot_pass_view'),
-    path('reset_pass/', ForgotPasswordView.as_view(), name='reset_pass_view'),
+    path('reset_pass/', ResetPasswordView.as_view(), name='reset_pass_view'),
 
 ]
