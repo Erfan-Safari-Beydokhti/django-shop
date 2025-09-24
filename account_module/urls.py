66640de0b954bda0119b60b601327de4da154aa7
Erfan <erfansafari.b.86@gmail.com>
2025-09-24 09:29:44 +0330

    create login template and import codes , add url for login view

--- account_module/urls.py
+++ account_module/urls.py
@@ -4,4 +4,5 @@ from .views import RegisterView, ActivateView
 urlpatterns = [
     path('register/',RegisterView.as_view(),name='register_view'),
     path('active_code/<email_active_code>/', ActivateView.as_view(), name='active_code'),
+    path('login', ActivateView.as_view(), name='login_view'),
 ]
