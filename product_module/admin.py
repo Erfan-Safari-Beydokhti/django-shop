2a23bb097c063b94937a758c3e9761b8ac4fd706
Erfan <erfansafari.b.86@gmail.com>
2025-09-27 21:16:16 +0330

    register product_module models in admin

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from product_module.models import Product, ProductCategory, ProductVisit, ProductTag, ProductGallery, ProductBrand
+
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductVisit)
+admin.site.register(ProductTag)
+admin.site.register(ProductGallery)
+admin.site.register(ProductBrand)
