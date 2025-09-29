bfab87aef44cde627edfc56b88b3f2ea682cfd97
Erfan <erfansafari.b.86@gmail.com>
2025-09-29 14:16:47 +0330

    edit query set in product list view for filter category

--- product_module/views.py
+++ product_module/views.py
@@ -1,5 +1,3 @@
-from lib2to3.fixes.fix_input import context
-
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views.generic import ListView
@@ -17,7 +15,12 @@ class ProductListView(ListView):
     paginate_by = 4
     ordering = ["-price"]
 
-
+    def get_queryset(self):
+        query=super(ProductListView, self).get_queryset()
+        category_name=self.kwargs.get('cat')
+        if category_name is not None:
+            query=query.filter(category__slug__iexact=category_name)
+        return query
 def product_categories_component(request:HttpRequest):
     main_categories=ProductCategory.objects.filter(parent=None,is_active=True).prefetch_related('children')
     context={'main_categories':main_categories}
