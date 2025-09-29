edb15df34078b9a26bf9950352410b27a9d74148
Erfan <erfansafari.b.86@gmail.com>
2025-09-29 14:12:55 +0330

    add url for categories to send slug from view

--- product_module/urls.py
+++ product_module/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import ProductListView
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list-view'),
+    path('cat/<str:cat>', ProductListView.as_view(), name='product-category-list-view'),
 ]
