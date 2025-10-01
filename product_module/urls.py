87feb2546c0c754b05a5cdf5593eb3b425547525
Erfan <erfansafari.b.86@gmail.com>
2025-10-01 10:28:32 +0330

    add url for filter brand

--- product_module/urls.py
+++ product_module/urls.py
@@ -3,4 +3,5 @@ from .views import ProductListView
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list-view'),
     path('cat/<str:cat>', ProductListView.as_view(), name='product-category-list-view'),
+    path('brand/<str:brand>', ProductListView.as_view(), name='product-brand-list-view'),
 ]
