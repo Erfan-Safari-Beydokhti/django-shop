d1a9b9d0d54e7f55ce0e8e0e8f3d476a76c3b9e8
Erfan <erfansafari.b.86@gmail.com>
2025-10-01 10:35:29 +0330

    implement product brand filter

--- product_module/views.py
+++ product_module/views.py
@@ -19,23 +19,31 @@ class ProductListView(ListView):
     ordering = ["-price"]
 
     def get_queryset(self):
-        query=super(ProductListView, self).get_queryset()
-        category_name=self.kwargs.get('cat')
-        price_min=self.request.GET.get('price_min')
-        price_max=self.request.GET.get('price_max')
+        query = super(ProductListView, self).get_queryset()
+        category_name = self.kwargs.get('cat')
+        brand_name = self.kwargs.get('brand')
+        price_min = self.request.GET.get('price_min')
+        price_max = self.request.GET.get('price_max')
         if category_name is not None:
-            query=query.filter(category__slug__iexact=category_name)
+            query = query.filter(category__slug__iexact=category_name)
         if price_min is not None:
-            query=query.filter(price__gte=price_min)
+            query = query.filter(price__gte=price_min)
         if price_max is not None:
-            query=query.filter(price__lte=price_max)
+            query = query.filter(price__lte=price_max)
+        if brand_name is not None:
+            query = query.filter(brand__slug__iexact=brand_name)
         return query
-def product_categories_component(request:HttpRequest):
-    main_categories=ProductCategory.objects.annotate(products_count=Count("product_categories")).filter(parent=None,is_active=True).prefetch_related(Prefetch('children',queryset=ProductCategory.objects.filter(is_active=True)))
-    context={'main_categories':main_categories}
-    return render(request,'product_module/component/product_categories_component.html',context)
 
-def product_brands_component(request:HttpRequest):
-    brand=ProductBrand.objects.filter(is_active=True)
-    context={'brand':brand}
-    return render(request,'product_module/component/product_brands_component.html',context)
+
+def product_categories_component(request: HttpRequest):
+    main_categories = ProductCategory.objects.annotate(products_count=Count("product_categories")).filter(parent=None,
+                                                                                                          is_active=True).prefetch_related(
+        Prefetch('children', queryset=ProductCategory.objects.filter(is_active=True)))
+    context = {'main_categories': main_categories}
+    return render(request, 'product_module/component/product_categories_component.html', context)
+
+
+def product_brands_component(request: HttpRequest):
+    brand = ProductBrand.objects.annotate(products_count=Count("product_brands")).filter(is_active=True)
+    context = {'brand': brand}
+    return render(request, 'product_module/component/product_brands_component.html', context)
