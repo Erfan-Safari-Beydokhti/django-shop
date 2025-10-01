bdf92352ebe081cce93efdd809f3564e73fe24f8
Erfan <erfansafari.b.86@gmail.com>
2025-10-01 17:11:18 +0330

    edit title of product to correct link to related detail

--- product_module/urls.py
+++ product_module/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
+
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list-view'),
     path('cat/<str:cat>', ProductListView.as_view(), name='product-category-list-view'),
     path('brand/<str:brand>', ProductListView.as_view(), name='product-brand-list-view'),
-    path('<slug:slug>', ProductListView.as_view(), name='product-detail-view'),
+    path('<slug:slug>', ProductDetailView.as_view(), name='product-detail-view'),
 ]
