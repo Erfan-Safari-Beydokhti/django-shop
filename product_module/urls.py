b327465541639f786ce82d966391c3ece2710550
Erfan <erfansafari.b.86@gmail.com>
2025-10-03 10:49:30 +0330

    add url for wishlist view

--- product_module/urls.py
+++ product_module/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, add_to_wishlist
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list-view'),
     path('cat/<str:cat>/', ProductListView.as_view(), name='product-category-list-view'),
     path('brand/<str:brand>/', ProductListView.as_view(), name='product-brand-list-view'),
+    path('wishlist/add/<int:product_id>', add_to_wishlist, name='add_to_wishlist'),
     path('<slug:slug>/', ProductDetailView.as_view(), name='product-detail-view'),
 ]
