fe1e462028bcb1733c8864acd496cfefa5c8f5a2
Erfan <erfansafari.b.86@gmail.com>
2025-10-03 10:42:44 +0330

    create model for wishlist with user,product,create_at fields

--- product_module/models.py
+++ product_module/models.py
@@ -126,3 +126,16 @@ class ProductVisit(models.Model):
     class Meta:
         verbose_name = "Product Visit"
         verbose_name_plural = "Product Visits"
+
+class WishList(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE,)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Create at")
+
+    class Meta:
+        unique_together = ("user", "product")
+        verbose_name = "Wish List"
+        verbose_name_plural = "Wish List"
+
+    def __str__(self):
+        return f"{self.user} / {self.product}"
