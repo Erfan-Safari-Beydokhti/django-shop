c9627e0b26fb023101487bbbc03979c25a1c6bbc
Erfan <erfansafari.b.86@gmail.com>
2025-10-04 11:49:16 +0330

    create form for Review model

---
+++ product_module/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from product_module.models import ProductReview
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = ProductReview
+        fields = ["text", "rating"]
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'input-text input-text--primary-style',"placeholder":"Enter review text ..."}),
+
+        }
