5639761b8374a7a9c6b641225252571b3b892d1a
Erfan <erfansafari.b.86@gmail.com>
2025-10-08 12:53:04 +0330

    the problem of visit sort is solve

--- utils/product_sort_service.py
+++ utils/product_sort_service.py
@@ -9,7 +9,7 @@ class ProductSortService:
         products_qs = Product.objects.filter(is_active=True,is_delete=False)
 
         if sort == 'Visit':
-            products_qs = products_qs.annotate(visit_count=Count('visit')).order_by('-visit_count')
+            products_qs = products_qs.annotate(visit_count=Count('product_visits')).order_by('-visit_count')
         elif sort == 'Rating':
             products_qs = products_qs.annotate(avg_rating=Avg('reviews__rating')).order_by('-avg_rating')
         elif sort == 'Lowest_p':
