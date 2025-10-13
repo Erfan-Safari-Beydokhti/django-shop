53eefed55a1cd472f0e6b10f62e6c86062208a93
Erfan <erfansafari.b.86@gmail.com>
2025-10-13 10:09:31 +0330

    set meta class and str , save function

--- blog_module/models.py
+++ blog_module/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from product_module.models import unique_slugify
 # Create your models here.
 
 class BlogCategory(models.Model):
@@ -7,4 +7,14 @@ class BlogCategory(models.Model):
     title = models.CharField(max_length=200, verbose_name='Category title')
     slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug',db_index=True,default='',null=True,blank=True)
     is_active = models.BooleanField(default=True, verbose_name='Is active')
+
+    class Meta:
+        verbose_name = 'Blog category'
+        verbose_name_plural = 'Blog categories'
+    def __str__(self):
+        return self.title
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = unique_slugify(self,self.title)
+        super(BlogCategory,self).save(*args, **kwargs)
 
