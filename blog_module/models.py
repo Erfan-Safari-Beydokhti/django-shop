62403512870f5db282a8d5d312c559756a3251e2
Erfan <erfansafari.b.86@gmail.com>
2025-10-13 10:57:27 +0330

    edit blog_module model to better performance

--- blog_module/models.py
+++ blog_module/models.py
@@ -9,7 +9,7 @@ from utils.unique_slugify_service import unique_slugify
 class BlogCategory(models.Model):
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE , verbose_name='Parent category')
     title = models.CharField(max_length=200, verbose_name='Category title')
-    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug',db_index=True,default='',null=True,blank=True)
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug',db_index=True,default='',blank=True)
     is_active = models.BooleanField(default=True, verbose_name='Is active')
 
     class Meta:
@@ -24,12 +24,12 @@ class BlogCategory(models.Model):
 
 class Blog(models.Model):
     title = models.CharField(max_length=200, verbose_name='Title',unique=True)
-    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug',db_index=True,default='',null=True,blank=True)
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug',db_index=True,default='',blank=True)
     image=models.ImageField(upload_to='images/blogs', verbose_name='Image' ,null=True,blank=True)
-    short_description = models.TextField(verbose_name='Short description',null=True,blank=True)
-    text = models.TextField(verbose_name='Text',null=True,blank=True)
+    short_description = models.TextField(verbose_name='Short description',blank=True)
+    text = models.TextField(verbose_name='Text',blank=True)
     selected_categories=models.ManyToManyField(BlogCategory,verbose_name='Selected category',related_name='blogs')
-    author = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='Author',related_name='blogs',null=True,blank=True,editable=False)
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,verbose_name='Author',related_name='blogs',null=True,blank=True,editable=False)
     created_at = models.DateTimeField(auto_now_add=True,verbose_name='Created at',editable=False)
     updated_at = models.DateTimeField(auto_now=True,verbose_name='Updated at',editable=False)
     is_active = models.BooleanField(default=True, verbose_name='Is active')
@@ -37,6 +37,7 @@ class Blog(models.Model):
     class Meta:
         verbose_name = 'Blog'
         verbose_name_plural = 'Blogs'
+        ordering = ['-created_at']
     def __str__(self):
         return self.title
     def save(self, *args, **kwargs):
@@ -55,6 +56,7 @@ class BlogComment(models.Model):
     class Meta:
         verbose_name = 'Blog comment'
         verbose_name_plural = 'Blog comments'
+        ordering = ['-created_at']
     def __str__(self):
         return f"{self.user}: {self.blog}"
 
