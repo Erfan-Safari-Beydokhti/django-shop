2e365a70ae685e0a43bd04fbd1299859077bc0aa
Erfan <erfansafari.b.86@gmail.com>
2025-10-13 10:21:31 +0330

    create initial blog model

--- blog_module/models.py
+++ blog_module/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from account_module.models import User
 from utils.unique_slugify_service import unique_slugify
 
 
@@ -19,4 +21,15 @@ class BlogCategory(models.Model):
         if not self.slug:
             self.slug = unique_slugify(self,self.title)
         super(BlogCategory,self).save(*args, **kwargs)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Title')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug',db_index=True,default='',null=True,blank=True)
+    image=models.ImageField(upload_to='images/blogs', verbose_name='Image' ,null=True,blank=True)
+    short_description = models.TextField(verbose_name='Short description',null=True,blank=True)
+    text = models.TextField(verbose_name='Text',null=True,blank=True)
+    selected_categories=models.ManyToManyField(BlogCategory,verbose_name='Selected category',related_name='blogs')
+    author = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='Author',related_name='blogs',null=True,blank=True,editable=False)
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Created at',editable=False)
+    is_active = models.BooleanField(default=True, verbose_name='Is active')
 
