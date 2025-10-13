1113ccd57492006bc9ee0357dce9f9f204dbb29f
Erfan <erfansafari.b.86@gmail.com>
2025-10-13 10:07:33 +0330

    create initial blog category

--- blog_module/models.py
+++ blog_module/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogCategory(models.Model):
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE , verbose_name='Parent category')
+    title = models.CharField(max_length=200, verbose_name='Category title')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Slug',db_index=True,default='',null=True,blank=True)
+    is_active = models.BooleanField(default=True, verbose_name='Is active')
+
