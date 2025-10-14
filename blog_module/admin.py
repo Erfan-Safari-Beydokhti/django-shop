6c5678c626bf4a1307b4aaa9cffe68cdae308436
Erfan <erfansafari.b.86@gmail.com>
2025-10-14 09:38:44 +0330

    register blogtag model to admin

--- blog_module/admin.py
+++ blog_module/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Blog,BlogComment,BlogCategory
+from .models import Blog,BlogComment,BlogCategory,BlogTag
 # Register your models here.
 
 
 admin.site.register(Blog)
 admin.site.register(BlogComment)
 admin.site.register(BlogCategory)
+admin.site.register(BlogTag)
