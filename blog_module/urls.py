81651d1ce0c8576120a8c7f8c1d6d6590417146d
Erfan <erfansafari.b.86@gmail.com>
2025-10-15 08:52:16 +0330

    set url for tag filter

--- blog_module/urls.py
+++ blog_module/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import BlogListView
 
-urlpatterns=[
-    path('',BlogListView.as_view(),name='blog_list'),
-    path('cat/<str:cat>',BlogListView.as_view(),name='blog-category-list-view'),
-
+urlpatterns = [
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('cat/<str:cat>', BlogListView.as_view(), name='blog-category-list-view'),
+    path('tag/<str:tag>', BlogListView.as_view(), name='blog-tag-list-view'),
 ]
