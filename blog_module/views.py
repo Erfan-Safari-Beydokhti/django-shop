720656dbf73c123329230c9afc66df58744db26b
Erfan <erfansafari.b.86@gmail.com>
2025-10-15 08:51:47 +0330

    change tag filter to partial view

--- blog_module/views.py
+++ blog_module/views.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.db.models import Prefetch, Count
-from blog_module.models import Blog, BlogCategory
+from blog_module.models import Blog, BlogCategory, BlogTag
 
 
 # Create your views here.
@@ -18,22 +18,31 @@ class BlogListView(ListView):
     ordering = ['-created_at']
 
     def get_queryset(self):
-        query=super(BlogListView, self).get_queryset()
-        category_name=self.kwargs.get('cat')
-        search=self.request.GET.get('search')
+        query = super(BlogListView, self).get_queryset()
+        category_name = self.kwargs.get('cat')
+        search = self.request.GET.get('search')
         if category_name is not None:
-            query=query.filter(selected_categories__slug__iexact=category_name)
+            query = query.filter(selected_categories__slug__iexact=category_name)
         if search is not None:
-            query=query.filter(title__icontains=search)
+            query = query.filter(title__icontains=search)
 
-        query=query.filter(is_active=True).prefetch_related('selected_categories','author')
+
+        query = query.filter(is_active=True).prefetch_related('selected_categories', 'author')
         return query
 
+
 def blog_categories_component(request: HttpRequest):
-    main_categories = BlogCategory.objects.annotate(blog_count=Count('blogs')).filter(is_active=True,parent=None).prefetch_related(Prefetch('children',queryset=BlogCategory.objects.filter(is_active=True)))
-    context={'main_categories':main_categories}
-    return render(request,'blog_module/component/blog_categories_component.html',context)
+    main_categories = BlogCategory.objects.annotate(blog_count=Count('blogs')).filter(is_active=True,
+                                                                                      parent=None).prefetch_related(
+        Prefetch('children', queryset=BlogCategory.objects.filter(is_active=True)))
+    context = {'main_categories': main_categories}
+    return render(request, 'blog_module/component/blog_categories_component.html', context)
+
+
 def blog_recent_post_component(request: HttpRequest):
-    recent_post=Blog.objects.filter(is_active=True).order_by('-created_at')[:3]
-    context={'recent_post':recent_post}
-    return render(request,'blog_module/component/blog_recent_post_component.html',context)
+    recent_post = Blog.objects.filter(is_active=True).order_by('-created_at')[:3]
+    context = {'recent_post': recent_post}
+    return render(request, 'blog_module/component/blog_recent_post_component.html', context)
+
+
+
