dcb9f32a906583832e2ec753f4ba924a8089a4fb
Erfan <erfansafari.b.86@gmail.com>
2025-10-15 09:06:13 +0330

    set url for blog detail view

--- blog_module/urls.py
+++ blog_module/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
     path('cat/<str:cat>', BlogListView.as_view(), name='blog-category-list-view'),
     path('tag/<str:tag>', BlogListView.as_view(), name='blog-tag-list-view'),
+    path('<slug:slug>' , BlogDetailView.as_view(), name='blog-detail'),
 ]
