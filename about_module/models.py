b09cff40934ee6c11857d033c3f17fc38ed4f009
Erfan <erfansafari.b.86@gmail.com>
2025-10-22 12:49:44 +0330

    create initial about model

--- about_module/models.py
+++ about_module/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class About(models.Model):
+    title = models.CharField(max_length=100,verbose_name='Title')
+    description = models.TextField(verbose_name='Description')
+
+
