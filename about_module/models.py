4940780036e6be825f4083f0c10747e677bf5d00
Erfan <erfansafari.b.86@gmail.com>
2025-10-22 12:50:27 +0330

    add str function , meta class to about model

--- about_module/models.py
+++ about_module/models.py
@@ -7,4 +7,9 @@ class About(models.Model):
     title = models.CharField(max_length=100,verbose_name='Title')
     description = models.TextField(verbose_name='Description')
 
+    def __str__(self):
+        return self.title
 
+    class Meta:
+        verbose_name = 'About'
+        verbose_name_plural = 'About'
