652d762a586260f09d15fa87b1d816a5dc2373df
Erfan <erfansafari.b.86@gmail.com>
2025-10-25 17:11:04 +0330

    register models on admin

--- about_module/admin.py
+++ about_module/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import About,TeamMember,ClientsFeedback
+
+admin.site.register(About)
+admin.site.register(TeamMember)
+admin.site.register(ClientsFeedback)
