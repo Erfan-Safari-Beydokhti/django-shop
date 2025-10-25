029023d7439d81f918a058684136cd876140479a
Erfan <erfansafari.b.86@gmail.com>
2025-10-25 17:19:57 +0330

    create CBV for all about detail

--- about_module/views.py
+++ about_module/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from about_module.models import About, TeamMember, ClientsFeedback
+
 
 # Create your views here.
 
 
-def about(request):
-    return render(request,'about_module/about.html')
+class AboutView(TemplateView):
+    model=About
+    template_name = 'about_module/about.html'
+    context_object_name = 'about'
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['team_members']=TeamMember.objects.all()
+        context['feedbacks']=ClientsFeedback.objects.all()
+        return context
+
+
