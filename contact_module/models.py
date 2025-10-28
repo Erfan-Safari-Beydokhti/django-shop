b63c7146089304ea9130c2c73040b17e5c6698e9
Erfan <erfansafari.b.86@gmail.com>
2025-10-28 09:55:43 +0300

    create model for contact messages

--- contact_module/models.py
+++ contact_module/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from account_module.models import User
+
+
 # Create your models here.
+class ContactMessages(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.subject
+    class Meta:
+        verbose_name_plural = 'Contact Messages'
+        verbose_name = 'Contact Message'
