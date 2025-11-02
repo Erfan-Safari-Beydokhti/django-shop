7d8665a31b479e14f35a6da4c3caf70428280891
Erfan <erfansafari.b.86@gmail.com>
2025-11-02 23:07:08 +0330

    create model for cart

--- cart_module/models.py
+++ cart_module/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from account_module.models import User
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='User',related_name='cart')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='Created')
+    updated = models.DateTimeField(auto_now=True,verbose_name='Updated')
+
+    def __str__(self):
+        return f"{self.id} , {self.user}"
+
+
