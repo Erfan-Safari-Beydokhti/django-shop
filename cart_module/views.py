7eee6d679b57f4951cb32b2406e5488209a0a421
Erfan <erfansafari.b.86@gmail.com>
2025-11-08 09:28:18 +0330

    add login required

--- cart_module/views.py
+++ cart_module/views.py
@@ -1,8 +1,7 @@
-from gc import get_objects
-
-from django.shortcuts import render
 from django.contrib.auth.views import login_required
-from django.shortcuts import get_object_or_404,redirect
+from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import render
+
 from cart_module.models import Cart, CartItem
 from product_module.models import Product
 
@@ -19,12 +18,12 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
     return redirect('cart_detail')
-
+@login_required()
 def cart_detail(request):
     cart,created = Cart.objects.get_or_create(user=request.user)
     return render(request,'cart_module/cart.html',{'cart':cart})
 
-
+@login_required()
 def remove_from_cart(request, item_id):
     item = get_object_or_404(CartItem, id=item_id,cart__user=request.user)
     item.delete()
