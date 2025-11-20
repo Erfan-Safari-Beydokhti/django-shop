af8d566d5da0efce498541ed8e532db782f0809a
Erfan <erfansafari.b.86@gmail.com>
2025-11-20 20:04:57 +0330

    .

--- dashboard_module/forms.py
+++ dashboard_module/forms.py
@@ -1,32 +1,32 @@
-from importlib.metadata import requires
-from random import choices
-
 from django import forms
 from django.contrib.auth import get_user_model
-from pyexpat.errors import messages
 
 User = get_user_model()
+
+
 class AddPhoneForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['phone']
         widgets = {
-            'phone': forms.TextInput(attrs={'class': 'input-text',}),
+            'phone': forms.TextInput(attrs={'class': 'input-text', }),
         }
-        
+
+
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name','last_name','birth_date','gender']
+        fields = ['first_name', 'last_name', 'birth_date', 'gender']
         widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'input-text input-text--primary-style',},),
-            'last_name': forms.TextInput(attrs={'class': 'input-text input-text--primary-style',}),
-            'birth_date': forms.DateInput(attrs={'type':'date','class': 'select-box select-box--primary-style',}),
-            'gender': forms.Select(attrs={'class': 'select-box select-box--primary-style',}),
+            'first_name': forms.TextInput(attrs={'class': 'input-text input-text--primary-style', }, ),
+            'last_name': forms.TextInput(attrs={'class': 'input-text input-text--primary-style', }),
+            'birth_date': forms.DateInput(attrs={'type': 'date', 'class': 'select-box select-box--primary-style', }),
+            'gender': forms.Select(attrs={'class': 'select-box select-box--primary-style', }),
         }
         error_messages = {
             'gender': {'required': 'Gender is required.'},
         }
+
     def clean_first_name(self):
         fname = self.cleaned_data.get("first_name")
         if not fname or fname.strip() == "":
--- dashboard_module/views.py
+++ dashboard_module/views.py
@@ -1,26 +1,38 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, UpdateView
+
 from account_module.models import User
 from dashboard_module.forms import AddPhoneForm, EditProfileForm
 
 
 # Create your views here.
 def dashboard(request):
-    return render(request,'dashboard_module/dashboard.html')
+    return render(request, 'dashboard_module/dashboard.html')
+
+
 def dash_address_add(request):
-    return render(request,'dashboard_module/dash_address_add.html')
+    return render(request, 'dashboard_module/dash_address_add.html')
+
+
 def dash_address_edit(request):
-    return render(request,'dashboard_module/dash_address_edit.html')
+    return render(request, 'dashboard_module/dash_address_edit.html')
+
+
 def dash_address_book(request):
-    return render(request,'dashboard_module/dash_address_book.html')
+    return render(request, 'dashboard_module/dash_address_book.html')
+
+
 def dash_address_make_default(request):
-    return render(request,'dashboard_module/dash_address_make_default.html')
+    return render(request, 'dashboard_module/dash_address_make_default.html')
+
+
 def dash_cancellation(request):
-    return render(request,'dashboard_module/dash_cancellation.html')
+    return render(request, 'dashboard_module/dash_cancellation.html')
+
+
 class EditProfileView(LoginRequiredMixin, UpdateView):
     model = User
     template_name = 'dashboard_module/dash_edit_profile.html'
@@ -40,32 +52,40 @@ class EditProfileView(LoginRequiredMixin, UpdateView):
                 messages.error(self.request, error)
         return super().form_invalid(form)
 
+
 def dash_manage_order(request):
-    return render(request,'dashboard_module/dash_manage_order.html')
+    return render(request, 'dashboard_module/dash_manage_order.html')
+
+
 def dash_my_order(request):
-    return render(request,'dashboard_module/dash_my_order.html')
+    return render(request, 'dashboard_module/dash_my_order.html')
+
+
 class MyProfileView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard_module/dash_my_profile.html"
+
     def get_context_data(self, **kwargs):
-        context=super().get_context_data(**kwargs)
-        user=self.request.user
-        context["full_name"]=user.get_full_name()
-        context["email"]=user.email
-        context["phone"]=getattr(user,"phone",'Please enter your mobile')
-        context["birthday"]=getattr(user,"birth_date",'Please enter your Birthday')
-        gen=user.gender
-        context["gender"]= 'Male' if gen=='M' else 'Female' if gen=='F' else None
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context["full_name"] = user.get_full_name()
+        context["email"] = user.email
+        context["phone"] = getattr(user, "phone", 'Please enter your mobile')
+        context["birthday"] = getattr(user, "birth_date", 'Please enter your Birthday')
+        gen = user.gender
+        context["gender"] = 'Male' if gen == 'M' else 'Female' if gen == 'F' else None
 
         return context
 
 
 def dash_track_order(request):
-    return render(request,'dashboard_module/dash_track_order.html')
+    return render(request, 'dashboard_module/dash_track_order.html')
+
 
 class AddPhoneView(LoginRequiredMixin, UpdateView):
     model = User
     template_name = 'dashboard_module/add_phone_number.html'
     form_class = AddPhoneForm
     success_url = reverse_lazy("dash-my-profile")
+
     def get_object(self):
         return self.request.user
