26a80bb3299ca009a690ed15626bf92b01e19dca
Erfan <erfansafari.b.86@gmail.com>
2025-11-25 20:10:01 +0330

    implement track order section

--- order_module/views.py
+++ order_module/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 from account_module.models import User
 from order_module.models import Order
@@ -8,10 +9,11 @@ from order_module.models import Order
 
 # Create your views here.
 
-class OrderListView(LoginRequiredMixin,ListView):
+class OrderListView(LoginRequiredMixin, ListView):
     model = Order
     context_object_name = 'orders'
     template_name = 'dashboard_module/dash_my_order.html'
+
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user).prefetch_related("items__product").order_by('-created')
 
@@ -24,21 +26,18 @@ class OrderDetailView(LoginRequiredMixin, DetailView):
     def get_object(self):
         return Order.objects.get(id=self.kwargs["id"], user=self.request.user)
 
-class TrackOrderView(TemplateView):
+
+class TrackOrderView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard_module/dash_track_order.html"
 
     def post(self, request, *args, **kwargs):
-        order_id = request.POST.get("order-id")
+        tracking_code = request.POST.get("order-id")
         email = request.POST.get("track-email")
 
         try:
-            user = User.objects.get(email__iexact=email)
-            order = Order.objects.get(order_id=order_id, user=user)
-        except:
-            return render(request, self.template_name, {
-                "error": "Order Not Found!"
-            })
-
-        return render(request, self.template_name, {
-            "order": order
-        })
+            user = User.objects.get(email=email)
+            order = Order.objects.get(payment_tracking_code=tracking_code, user=user)
+        except (User.DoesNotExist, Order.DoesNotExist):
+            messages.error(request, "Order Not Found!")
+            return redirect('track-order')
+        return redirect('order-detail', order.id)
