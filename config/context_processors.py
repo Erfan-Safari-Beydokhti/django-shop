8848e1f33e33d9847769aeec875e6842e5f26371
Erfan <erfansafari.b.86@gmail.com>
2025-12-07 06:46:27 +0330

    implement Mega menu for each main and sub categories

--- config/context_processors.py
+++ config/context_processors.py
@@ -3,5 +3,5 @@ from product_module.models import ProductCategory
 
 def header_categories(request):
     return {
-        'categories':ProductCategory.objects.all(),
+        'categories':ProductCategory.objects.filter(is_active=True,is_delete=False,parent__isnull=True),
     }
