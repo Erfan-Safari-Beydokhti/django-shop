d2d472f8f35bc0781d3f4cbe40fb10a1c9d4f60f
Erfan <erfansafari.b.86@gmail.com>
2025-12-18 12:17:30 +0330

    create slider model on home module

--- home_module/models.py
+++ home_module/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class HomeSlider(models.Model):
+    title = models.CharField(max_length=100,verbose_name='Title')
+    image=models.ImageField(upload_to='images/sliders',verbose_name='Image')
+    link=models.URLField(blank=True,verbose_name='Link')
+    is_active=models.BooleanField(default=True,verbose_name='Active')
+    order=models.PositiveIntegerField(default=0,verbose_name='Order')
+
+    class Meta:
+        ordering=['order']
+        verbose_name='Slider'
+        verbose_name_plural='Sliders'
+    def __str__(self):
+        return self.title
