ec977af1c63ea1c8f364c3bd8b0b69d99e87a5df
Erfan <erfansafari.b.86@gmail.com>
2025-12-18 11:31:55 +0330

    complete header section except checkout

--- config/context_processors.py
+++ config/context_processors.py
@@ -1,7 +1,18 @@
+from django.contrib import messages
+
+from cart_module.models import CartItem, Cart
 from product_module.models import ProductCategory
 
 
 def header_categories(request):
+    cart = Cart.objects.get(user=request.user)
     return {
         'categories':ProductCategory.objects.filter(is_active=True,is_delete=False,parent__isnull=True),
+        'cart_items' : existitems(request, cart),
+        'sub_total':float(cart.total_price())
     }
+def existitems(request, cart):
+    if cart.items.exists():
+        return CartItem.objects.filter(cart=cart, cart__user=request.user)
+    else:
+        messages.error(request, 'No cart items')
