3add1da8071bb08250232a4ee7e683e0a3d3a114
Erfan <erfansafari.b.86@gmail.com>
2025-12-31 16:44:34 +0330

    implement sort section but has a problem

--- home_module/urls.py
+++ home_module/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from home_module.views import IndexView
+from home_module.views import IndexView, HomeProductTabAjaxView
 
 urlpatterns = [
     path('',IndexView.as_view(), name='home'),
+    path('ajax',HomeProductTabAjaxView.as_view(), name='home-products-ajax'),
+
 ]
--- home_module/views.py
+++ home_module/views.py
@@ -1,5 +1,10 @@
+from lib2to3.fixes.fix_input import context
+from tempfile import template
+
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.template.loader import render_to_string
+from django.views.generic import TemplateView, View
 
 from home_module.models import HomeSlider
 from product_module.models import ProductCategory, Product
@@ -15,3 +20,31 @@ class IndexView(TemplateView):
         context["phones"]=Product.objects.prefetch_related("category").filter(is_active=True,category__slug='phone')[:6]
         context["laptops"]=Product.objects.prefetch_related("category").filter(is_active=True,category__slug='laptop')[:6]
         return context
+
+class HomeProductTabAjaxView(View):
+    def get(self, request, *args, **kwargs):
+        category=request.GET.get('category')
+        sort=request.GET.get('sort','Newest')
+        template = request.GET.get('template')
+
+        qs=Product.objects.filter(category__slug=category,is_active=True).distinct()
+
+        if sort == 'Newest':
+            qs = qs.order_by('-id')
+        elif sort == 'Latest':
+            qs = qs.order_by('id')
+        elif sort == 'Visit':
+            qs = qs.order_by('-product_visits')
+        elif sort == 'Rating':
+            qs = qs.order_by('-reviews')
+        elif sort == 'Lowest_p':
+            qs = qs.order_by('price')
+        elif sort == 'Highest_p':
+            qs = qs.order_by('-price')
+
+        products=qs[:8]
+        html = render_to_string(f'home_module/includes/{template}',
+            {'products': products},
+            request=request
+        )
+        return JsonResponse({'html':html})
