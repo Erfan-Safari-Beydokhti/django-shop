898dedaf3814e035ce45ffe0d02f3866af3fd98b
Erfan <erfansafari.b.86@gmail.com>
2026-01-02 11:08:28 +0330

    complete latest blog section in home

--- home_module/views.py
+++ home_module/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Count
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.views.generic import TemplateView, View
 
+from blog_module.models import Blog
 from home_module.models import HomeSlider
 from product_module.models import ProductCategory, Product
 
@@ -15,5 +17,6 @@ class IndexView(TemplateView):
         context["electronic_categories"]=ProductCategory.objects.filter(is_active=True,show_on_home=True)[:4]
         context["phones"]=Product.objects.prefetch_related("category").filter(is_active=True,category__slug='phone')[:6]
         context["laptops"]=Product.objects.prefetch_related("category").filter(is_active=True,category__slug='laptop')[:6]
+        context["blogs"]=Blog.objects.filter(is_active=True).annotate(comments_count=Count('comments')).select_related('author').prefetch_related('selected_categories','tag').order_by('-created_at')[:3]
         return context
 
