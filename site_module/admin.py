3db4e942bd6d24ff0ebd10c8e553954100fc9e81
Erfan <erfansafari.b.86@gmail.com>
2026-01-03 19:13:52 +0330

    register new model on admin

--- site_module/admin.py
+++ site_module/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import SiteSettings,FooterLinkBox,FooterLink
 # Register your models here.
+
+admin.site.register(SiteSettings)
+admin.site.register(FooterLinkBox)
+admin.site.register(FooterLink)
